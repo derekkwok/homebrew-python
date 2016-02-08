3d8a07aca2f478587a82cf92d08eac4ad58f08e1
derekkwok <derek.kai.chun.kwok@gmail.com>
2016-02-08 13:23:55 -0500

    Added python 2.6 and 3.1 formula

---
+++ python26.rb
@@ -0,0 +1,22 @@
+class Python26 < Formula
+  desc "Interpreted, interactive, object-oriented programming language"
+  homepage "https://www.python.org/"
+  url "https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz"
+  version "2.6.9"
+  sha256 "7277b1285d8a82f374ef6ebaac85b003266f7939b3f2a24a3af52f9523ac94db"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"bin").install_symlink bin/"python2.6"
+    (HOMEBREW_PREFIX/"bin").install_symlink bin/"python2.6-config"
+  end
+
+end
---
+++ python31.rb
@@ -0,0 +1,21 @@
+class Python31 < Formula
+  desc "Interpreted, interactive, object-oriented programming language"
+  homepage "https://www.python.org/"
+  url "https://www.python.org/ftp/python/3.1.5/Python-3.1.5.tgz"
+  version "3.1.5"
+  sha256 "d12dae6d06f52ef6bf1271db4d5b4d14b5dd39813e324314e72b648ef1bc0103"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"bin").install_symlink bin/"python3.1"
+    (HOMEBREW_PREFIX/"bin").install_symlink bin/"python3.1-config"
+  end
+
+end
