0f66def499c63d9ee6015c8a102796d20fe3b335
derekkwok <derek.kai.chun.kwok@gmail.com>
2016-02-08 08:40:17 -0500

    Fixed sha256 value for python32 formula

--- python32.rb
+++ python32.rb
@@ -3,7 +3,7 @@ class Python32 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.2.6/Python-3.2.6.tgz"
   version "3.2.6"
-  sha256 "0a58ad1f1def4ecc90b18b0c410a3a0e1a48cf7692c75d1f83d0af080e5d2034"
+  sha256 "fc1e41296e29d476f696303acae293ae7a2310f0f9d0d637905e722a3f16163e"
 
   def install
     system "./configure", "--disable-debug",
